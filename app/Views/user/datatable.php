35963e74ee452e70e2b1fab7ce794cfa425bfe0a
Bali Merta <made.b4li@gmail.com>
2022-04-05 14:27:17 +0800

    Update datatable.php

--- app/Views/user/datatable.php
+++ app/Views/user/datatable.php
@@ -249,7 +249,7 @@
                 },
             },
             "columnDefs": [{
-                    "targets": [0, 3, 5, -1],
+                    "targets": [0, 3, 4, 5, -1],
                     "orderable": false,
                 },
                 {
