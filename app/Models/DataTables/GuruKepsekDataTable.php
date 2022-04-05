460f27f558c785c9489f401eef58d08e9c364a71
bali.appkey <bali.appkey@gmail.com>
2022-04-05 15:07:37 +0800

    Fix status guru

--- app/Models/DataTables/GuruKepsekDataTable.php
+++ app/Models/DataTables/GuruKepsekDataTable.php
@@ -68,7 +68,7 @@ class GuruKepsekDataTable extends Model
         $this->getDatatablesQuery();
         if ($this->request->getPost('length') != -1)
             $this->dt->limit($this->request->getPost('length'), $this->request->getPost('start'));
-        $this->dt->where('status', 'aktif');
+        // $this->dt->where('status', 'aktif');
         $query = $this->dt->get();
         return $query->getResult();
     }
